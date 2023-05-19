7ebf7f7129091cbaf8bce85e267849480f4dcc66
vishal ashok kalaskar (TTL) <vishalak.ttl@tatamotors.com>
2023-05-19 23:34:05 +0530

    workingcontroller

--- FileUploadOnServerforstudycodeforexceptionandlogic/src/main/java/com/fileupload/service/test/FileUploadController.java
+++ FileUploadOnServerforstudycodeforexceptionandlogic/src/main/java/com/fileupload/service/test/FileUploadController.java
@@ -1,5 +1,9 @@
-package com.fileupload.service.test;
+package com.map.service.fileupload;
 
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,44 +14,130 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fileupload.service.helper.FileUploadHelper;
+//import com.fileupload.service.helper.FileUploadHelper;
 
 @RestController
 public class FileUploadController {
 
 	@Autowired
 	private FileUploadHelper  fileUploadHelper;
-	@PostMapping(value="/jsonFileUpload",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},
+	
+	@PostMapping("/dev/uploadfile") 
+	  public ResponseEntity<Map<String,Object>> handleFileUpload(@RequestParam("file") MultipartFile file ) {
+
+	    String fileName = file.getOriginalFilename();
+	    try 
+	    {
+	     file.transferTo( new File("D:\\VishalK\\uploadfile" + fileName));
+	      //boolean f= fileUploadHelper.uploadFile(file);
+		  //System.out.println( "Value of F is   "+f);
+	      System.out.println("file upload succefully");
+	      System.out.println("file Name is "+fileName);
+	      Map<String,Object> res = new HashMap<>();
+	      res.put("success",true);
+	      res.put("message", "File uploaded successfully");
+	   // return ResponseEntity.ok("File uploaded successfully");
+	     return ResponseEntity.ok(res);
+	    }
+	    catch (Exception e) 
+	    {
+	    	Map<String,Object> res = new HashMap<>();
+		      res.put("success",false);
+		      res.put("message", "File uploaded Error");
+	      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+	    } 
+	  }
+
+	
+	@PostMapping(value="/dev/jsonFileUpload",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},
 			produces= {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<String>  UPDfileUpload(@RequestParam("file") MultipartFile file){
+	public ResponseEntity<Map<String,Object>> UPDfileUpload(@RequestParam("file") MultipartFile file){
 		
 		System.out.println("File name"+file.getOriginalFilename());
 		System.out.println("File Size"+file.getSize());
 		System.out.println("File ContentType"+file.getContentType());
 		try {
 			if (file.isEmpty()) {
-         return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Empty File,Please upload file with content");
+				Map<String,Object> res = new HashMap<>();
+			      res.put("success",false);
+			      res.put("message", "Empty File,Please upload file with content");
+		      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+          //return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Empty File,Please upload file with content");
    
 			}
 			if(!file.getContentType().equals("application/json")) {
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only Json File allowed");
+				Map<String,Object> res = new HashMap<>();
+			      res.put("success",false);
+			      res.put("message", "Only Json File allowed");
+		      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+				//return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only Json File allowed");
 			}
 			
 			boolean f= fileUploadHelper.uploadFile(file);
 			System.out.println( "Value of F is   "+f);
 			if(f) {
 				System.out.println("File Uploaded successfuly ............");
-				return ResponseEntity.ok("File Upload Successfully");
+				Map<String,Object> res = new HashMap<>();
+			      res.put("success",true);
+			      res.put("message", "File uploaded successfully");
+			     return ResponseEntity.ok(res);
+				//return ResponseEntity.ok("File Upload Successfully");
 			}
 			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}		
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something Went wrong Please try again");		
+		Map<String,Object> res = new HashMap<>();
+	      res.put("success",false);
+	      res.put("message", "Something Went wrong Please try again");
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+		//return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something Went wrong Please try again");		
 	}
 	
-	@PostMapping("/updFileUpload")
-	public ResponseEntity<String>  JsonfileUpload(@RequestParam("updfile") MultipartFile file){
+	@PostMapping("/dev/updFileUpload")
+	public ResponseEntity<Map<String,Object>>  JsonfileUpload(@RequestParam("updfile") MultipartFile file){
+		
+		System.out.println("File name"+file.getOriginalFilename());
+		System.out.println("File Size"+file.getSize());
+		System.out.println("File ContentType"+file.getContentType());
+		try {
+			if (file.isEmpty()) {
+				Map<String,Object> res = new HashMap<>();
+			      res.put("success",false);
+			      res.put("message", "Empty File,Please upload file with content");
+		      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+         //return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Empty File,Please upload file with content");
+   
+			}
+			if(!file.getContentType().equals("text/csv")) {
+				  Map<String,Object> res = new HashMap<>();
+			      res.put("success",false);
+			      res.put("message", "Only .CSV File allowed");
+		          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+				//return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only .CSV File allowed");
+			}
+			
+			boolean f= fileUploadHelper.uploadFile(file);
+			if(f) {
+				System.out.println("File Uploaded successfuly ............");
+				Map<String,Object> res = new HashMap<>();
+			      res.put("success",true);
+			      res.put("message", "File uploaded successfully");
+			     return ResponseEntity.ok(res);
+				//return	ResponseEntity.ok("File Upload Successfully");
+		}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}		
+		Map<String,Object> res = new HashMap<>();
+	      res.put("success",false);
+	      res.put("message", "Something Went wrong Please try again");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+		//return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something Went wrong Please try again");		
+	}
+	@PostMapping("/dev/txtFileUpload")
+	public ResponseEntity<String>  plainTextfileUpload(@RequestParam("txtfile") MultipartFile file){
 		
 		System.out.println("File name"+file.getOriginalFilename());
 		System.out.println("File Size"+file.getSize());
@@ -57,8 +147,8 @@ public class FileUploadController {
          return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Empty File,Please upload file with content");
    
 			}
-			if(!file.getContentType().equals("text/csv")) {
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only .CSV File allowed");
+			if(!file.getContentType().equals("text/plain")) {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only plain text File allowed");
 			}
 			
 			boolean f= fileUploadHelper.uploadFile(file);
@@ -72,4 +162,5 @@ public class FileUploadController {
 		}		
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something Went wrong Please try again");		
 	}
+
 }
